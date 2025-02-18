d088e46775c4ed3dfa55d56c85a5415b83c41b2c
fatihademtas0 <fatihademtas@outlook.com>
2025-02-18 16:48:13 +0300

    installation livewire

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         './resources/**/*.vue',
         'node_modules/preline/dist/*.js',
     ],
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
