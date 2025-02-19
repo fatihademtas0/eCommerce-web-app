d0008e4ba891d34612e3216a9b99a9763f60ad01
fatihademtas0 <fatihademtas@outlook.com>
2025-02-19 19:07:48 +0300

    blade components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Inverizo' }}</title>
-        <livewire:styles />
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="bg-slate-200 dark:bg-slate-700">
-        <main>
-            {{ $slot }}
-        </main>
+    <title>{{ $title ?? 'Inverizo' }}</title>
 
-        <livewire:scripts />
-    </body>
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-slate-200 dark:bg-slate-700">
+    @livewire('partials.navbar')
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    @livewire('partials.footer')
+    @livewireScripts
+</body>
 </html>
