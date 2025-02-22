7ffdd19f2a7ab7e5143409f289a6e22415c9d429
fatihademtas0 <fatihademtas@outlook.com>
2025-02-22 22:30:15 +0300

    filtering

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,22 +6,66 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-#[Title('Products Page - Inverizo')]
 
+#[Title('Products Page - Inverizo')]
 class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 50;
+    public $isFiltered = false; // Filtrenin aktif olup olmadığını takip eder
+
+    public function updatedPriceRange()
+    {
+        $this->isFiltered = true; // Kullanıcı price_range değiştirdiğinde filtreyi aktif et
+    }
+
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
-        return view('livewire.products-page' , [
-            'products' => $productQuery->paginate(6),
-            'brands' => Brand::where('is_active', 1)->get(['id', 'name' , 'slug']),
-            'categories' => Category::where('is_active', 1)->get(['id', 'name' , 'slug']),
+
+        if (!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if (!empty($this->selected_brands)) {
+            $productQuery->whereIn('brand_id', $this->selected_brands);
+        }
+
+        if ($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+
+        if ($this->on_sale) {
+            $productQuery->where('on_sale', 1);
+        }
+
+        if ( $this->isFiltered &&$this->price_range)
+        {
+            $productQuery->whereBetween('price' , [0 , $this->price_range]);
+        }
+
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(9),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         ]);
     }
 }
