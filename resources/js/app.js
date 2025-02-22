d5ec5efb3e3001f20f0b1ea7b58125134c2fa220
fatihademtas0 <fatihademtas@outlook.com>
2025-02-22 17:20:15 +0300

    home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,6 @@
 import './bootstrap';
 import 'preline'
+
+document.addEventListener('livewire:navigated', () => {
+    window.HSStaticMethods.autoInit(); // when navigated this will initialize all preline components whis requires javascript code
+})
