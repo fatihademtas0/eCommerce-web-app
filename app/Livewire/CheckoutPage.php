aa0ee0a3c76acb73347c0f1e785f8f2061ae814b
fatihademtas0 <fatihademtas@outlook.com>
2025-03-07 02:39:18 +0300

    check out page

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,60 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+
+    public function placeOrder()
+    {
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required'
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $tax = 0;
+
+        $shipping = 0;
+
+        $final_total = 0;
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+
+        $this->tax = $grand_total * 0.2;
+
+        $this->shipping = $grand_total * 0.01;
+
+        $this->final_total = $grand_total + $this->tax + $this->shipping;
+
+        return view('livewire.checkout-page', [
+            'grand_total' => $grand_total,
+            'cart_items' => $cart_items,
+            'final_total' => $this->final_total,
+            'tax' => $this->tax,
+            'shipping' => $this->shipping,
+        ]);
     }
 }
