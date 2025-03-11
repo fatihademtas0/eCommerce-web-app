45a37db8fd188502f62b80f5bc32fbd2709580b6
fatihademtas0 <fatihademtas@outlook.com>
2025-03-12 01:12:29 +0300

    success page

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Mail\OrderPlaced;
 use App\Models\Address;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 use Stripe\Checkout\Session;
@@ -125,6 +127,8 @@ class CheckoutPage extends Component
         $address->save();
 
         $order->items()->createMany($cart_items);
+
+        Mail::to(request()->user())->send(new OrderPlaced($order));
 
         CartManagement::clearCartItemsFromCookie();
 
--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -2,12 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
+#[Title('Success - Inverizo')]
 class SuccessPage extends Component
 {
+    #[Url]
+    public $session_id;
+
     public function render()
     {
-        return view('livewire.success-page');
+        $latest_order = Order::with('address')->where('user_id', auth()->user()->id)->latest()->first();
+
+        $final_total = 0;
+
+        if ($latest_order) {
+            $tax = $latest_order->grand_total * 0.2;
+            $shipping = $latest_order->grand_total * 0.01;
+            $final_total = $latest_order->grand_total + $tax + $shipping;
+        }
+
+        if ($this->session_id) {
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $session_info = Session::retrieve($this->session_id);
+
+            if ($session_info->payment_status != 'paid') {
+                $latest_order->payment_status = 'failed';
+
+                $latest_order->save();
+
+                return redirect()->route('cancel');
+            } else if ($session_info->payment_status == 'paid') {
+                $latest_order->payment_status = 'paid';
+                $latest_order->save();
+            }
+        }
+
+        return view('livewire.success-page', [
+            'order' => $latest_order,
+            'final_total' => $final_total,
+            'tax' => $tax,
+            'shipping' => $shipping
+        ]);
     }
 }
